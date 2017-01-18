5287f5c2134c00c902e3282f2a069ee6392647d4
EGOIST <0x142857@gmail.com>
2017-01-18 13:19:12 +0800

    set modules to false

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('path')
 
 const presets = [
   // latest ECMAScript features
-  require.resolve('babel-preset-latest'),
+  [require.resolve('babel-preset-latest'), {
+    es2015: {
+      // do not transform to commonjs format, let webpack 2 or rollup handle this
+      modules: false
+    }
+  }],
   // vue jsx
   require.resolve('babel-preset-vue'),
   // stage-3 features
