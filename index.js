3d08ae8d640a1cd155bd774d567cfaa2bd379413
EGOIST <0x142857@gmail.com>
2017-04-06 22:54:08 +0800

    add polyfill and helpers, fixed #1

--- index.js
+++ index.js
@@ -15,35 +15,19 @@ const presets = [
     targets: {
       ie: 9,
       uglify: true
-    },
-    useBuiltIns: true
+    }
   }],
   // vue jsx
   require.resolve('babel-preset-vue')
 ]
 
 const plugins = [
-  // For vue-class-component
-  // class App extends Vue { msg = 123 }
-  require('babel-plugin-transform-class-properties'),
   // Polyfills the runtime needed for async/await and generators
   [require.resolve('babel-plugin-transform-runtime'), {
-    helpers: false,
-    polyfill: false,
-    regenerator: true,
     // Resolve the Babel runtime relative to the config.
     moduleName: path.dirname(require.resolve('babel-runtime/package'))
   }],
-  [require('babel-plugin-transform-object-rest-spread'), {
-    // Disable babel helper function
-    // {...a} => Object.assign({}, a)
-    // You need to ship your own Object.assign polyfill
-    useBuiltIns: true
-  }],
-  [require('babel-plugin-transform-regenerator'), {
-    // babel-preset-env already transforms async function to generator
-    async: false
-  }],
+  require('babel-plugin-transform-object-rest-spread'),
   // For dynamic import that you will use a lot in code-split
   require.resolve('babel-plugin-syntax-dynamic-import')
 ]
